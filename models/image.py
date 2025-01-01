4bcf7c7047b01e49adcfd6664a74f885620ef1ef
YOUNESSE25 <younessehaouas@gmail.com>
2025-01-01 01:54:20 +0100

    updating app/image.py

--- models/image.py
+++ models/image.py
@@ -11,3 +11,27 @@ class Image:
         self.filename = filename
         self.file_content = file_content
         self.date_created = date_created or datetime.utcnow()
+
+    def save(self):
+        """ saving/updating image. """
+        data_img = {
+            "img_id": self.img_id,
+            "uploaded_by": self.uploaded_by,
+            "filename": self.filename,
+            "file_content": self.file_content,
+            "date_created": self.date_created,
+        }
+        if self._id:
+            db.images.replace_one({"_id": self._id}, data_img)
+        else:
+            result = db.images.insert_one(data_img)
+            self._id = result.inserted_id
+
+    @classmethod
+    def find_by_user(cls, user_id):
+        images = db.images.find({"uploaded_by": user_id}).sort("date_created", -1)
+        return [cls(**image) for image in images]
+    
+    @classmethod
+    def creating_by_objectid(cls, uploaded_by, filename, file_content):
+        return cls(uploaded_by=uploaded_by, filename=filename, file_content=file_content)
