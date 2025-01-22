ea8c07052f4eaeb8cc6b3cdb7d1885c92262ce4d
EL BAHTOURI <elbahtouri@192.168.1.28>
2025-01-22 02:30:38 +0100

    add the class of the payment test of creation

--- tests/test_payment_routes.py
+++ tests/test_payment_routes.py
@@ -6,3 +6,16 @@ class TestPaymentRoutes(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.client = app.test_client()
+
+    @patch('paypalrestsdk.Payment')
+    def test_payment_creation(self, mock_payment):
+        """Test PayPal payment creation"""
+        mock_payment_instance = Mock()
+        mock_payment_instance.create.return_value = True
+        mock_payment_instance.links = [
+            Mock(rel='approval_url', href='http://paypal.com/approve')
+        ]
+        mock_payment.return_value = mock_payment_instance
+
+        response = self.client.get('/pay')
+        self.assertEqual(response.status_code, 302)
